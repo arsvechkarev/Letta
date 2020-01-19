00911f81a017018b92b0064705e7f5f2a9ea74c9
arsvechkarev <arsvechkarev@gmail.com>
2020-01-20 00:28:53 +0300

    Added first capturing result session

---
+++ app/src/main/java/com/arsvechkarev/cameram/Extensions.kt
@@ -0,0 +1,15 @@
+package com.arsvechkarev.cameram
+
+import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+const val TAG = "CameraM"
+
+fun Fragment.log(text: String) {
+  Log.d(TAG, text)
+}
+
+fun Fragment.showToast(text: String) {
+  Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+}
--- app/src/main/java/com/arsvechkarev/cameram/MainActivity.kt
+++ app/src/main/java/com/arsvechkarev/cameram/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.arsvechkarev.cameram
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
   
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    ActivityCompat.requestPermissions(
+      this,
+      arrayOf(CAMERA, READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE),
+      1
+    )
+  }
+  
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    
+    supportFragmentManager.beginTransaction()
+      .replace(android.R.id.content, CameraFragment())
+      .commit()
   }
 }
